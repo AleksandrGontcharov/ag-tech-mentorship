3ee5c378f43132cbcbdf8b97f3f9328f30517ed2
Aleksandr Gontcharov <alexgoncharov@gmail.com>
2023-02-20 16:12:04 -0800

    Update blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -77,7 +77,7 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          { to: '/blog', label: 'Blog', position: 'left' },
+          { to: '/blog', label: 'Updates', position: 'left' },
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
