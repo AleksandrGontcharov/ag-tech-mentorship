b1e7b398411eb8c9c5d71c627085e1f17fe3ca16
Aleksandr Gontcharov <alexgoncharov@gmail.com>
2023-02-20 19:26:47 -0800

    change pictures

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,38 +10,35 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'Learn the Fundamentals',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Master the essential tools and skills that will serve you for years to come.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Hands-on learning',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Learn through workshop style events and one-on-one mentorship.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Join a Supportive Community',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        I believe that mentorship and support is a rare gift that can change your life.
       </>
     ),
   },
 ];
 
-function Feature({title, Svg, description}: FeatureItem) {
+function Feature({ title, Svg, description }: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
